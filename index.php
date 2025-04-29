c2b9c4ae353269b5068014f1864e1d98bc73c23a
Sayyid Fayadh <sayyidfayadh5@gmail.com>
2025-04-29 09:39:32 +0530

    changes in header

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laundrsitc</title>
+    <title>Laundristic</title>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/fontawesome.min.css">
     <link rel="stylesheet" href="assets/css/venobox.min.css">
